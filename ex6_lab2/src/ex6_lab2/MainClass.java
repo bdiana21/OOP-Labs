9ee9481a4a130f7222db0d7afde262de97d698d5
DESKTOP-JER9P2P\user <boitor.diana21@gmail.com>
2023-10-18 13:35:31 +0300

    problema suplimentara

---
+++ ex6_lab2/src/ex6_lab2/MainClass.java
@@ -0,0 +1,23 @@
+package ex6_lab2;
+import java.math.BigInteger;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		BigInteger total=BigInteger.ZERO;
+		int tabla_sah=64;
+		for(int i=1;i<=tabla_sah;i++) {
+			BigInteger s=BigInteger.ONE;
+			for(int j=1;j<=i-1;j++ ) {
+				 //s=s*2;
+				s= s.multiply(BigInteger.valueOf(2));
+			}
+			//System.out.println(s);
+			total=total.add(s);
+			//total=total+s;
+			
+		}
+		System.out.println("total boabe de grau " + total );
+		
+	}
+}
